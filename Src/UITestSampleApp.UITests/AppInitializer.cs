3dace436ed8280458bb37d6884fe47898dff9df4
Adam Barlow <dadambarlow@gmail.com>
2018-07-11 14:05:40 -0700

    uitest update

--- Src/UITestSampleApp.UITests/AppInitializer.cs
+++ Src/UITestSampleApp.UITests/AppInitializer.cs
@@ -4,12 +4,15 @@ namespace UITestSampleApp.UITests
 {
     static class AppInitializer
     {
-        public static IApp StartApp(Platform platform)
+		public static IApp StartApp(Platform platform)
         {
             switch (platform)
             {
                 case Platform.Android:
-                    return ConfigureApp.Android.StartApp();
+					return ConfigureApp
+						.Android
+						.ApkFile(@"/../../../UITestSampleApp2/Src/UITestSampleApp.Droid/bin/Debug/*.apk")
+						.StartApp();
 
                 case Platform.iOS:
                     return ConfigureApp.iOS.StartApp();
